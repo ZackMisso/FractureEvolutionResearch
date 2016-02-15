b94f62f21039f1f43de93423ac1df46b23257d40
ZackMisso <zack441@mac.com>
2016-02-15 11:55:52 -0600

    Implemented Edge

---
+++ edge.cpp
@@ -0,0 +1,28 @@
+#include "edge.h"
+
+Edge::Edge(Point2 one,Point2 two) {
+  first.xpos = one.xpos;
+  first.ypos = one.ypos;
+  second.xpos = two.xpos;
+  second.ypos = two.ypos;
+}
+
+Edge::Edge(float x,float y,float xx,float yy) {
+  first.xpos = x;
+  first.ypos = y;
+  second.xpos = xx;
+  second.ypos = yy;
+}
+
+Point2 Edge::getFirst() { return first; }
+Point2 Edge::getSecond() { return second; }
+
+void Edge::setFirst(Point2 point) {
+  first.xpos = point.xpos;
+  first.ypos = point.ypos;
+}
+
+void Edge::setSecond(Point2 point) {
+  second.xpos = point.xpos;
+  second.ypos = point.ypos
+}
---
+++ edge.h
@@ -0,0 +1,21 @@
+#ifndef __EDGE_H__
+#define __EDGE_H__
+
+#include "point2.h"
+
+class Edge {
+private:
+  Point2 first;
+  Point2 second;
+public:
+  Edge(Point2 one,Point2 two);
+  Edge(float x,float y,float xx,float yy);
+  // getters
+  Point2 getFirst();
+  Point2 getSecond();
+  // setters
+  void setFirst(Point2 point);
+  void setSecond(Point2 point);
+};
+
+#endif
